1bac5ef9750b8df1d6f8348b175133d44464caf7
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-05-29 10:37:29 +0530

    done

--- app/src/main/java/com/example/lab5/Database/DBHandler.java
+++ app/src/main/java/com/example/lab5/Database/DBHandler.java
@@ -2,10 +2,16 @@ package com.example.lab5.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DBHandler extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "UserInfo.db";
 
@@ -38,5 +44,97 @@ public class DBHandler extends SQLiteOpenHelper {
         long insertReturn = db.insert(UserMaster.Users.TABLE_NAME,null,contentValues);
 
         return insertReturn;
+    }
+
+    public List ReadAllInfo(){
+        SQLiteDatabase db = getReadableDatabase();
+
+    String[] projection = {
+            UserMaster.Users._ID,
+            UserMaster.Users.COLUMN_NAME_USER_NAME,
+            UserMaster.Users.COLUMN_NAME_PASSWORD
+    };
+    String sortOrder =
+            UserMaster.Users.COLUMN_NAME_USER_NAME + " DESC ";
+
+    Cursor cursor = db.query(
+            UserMaster.Users.TABLE_NAME,   // The table to query
+            projection,             // The array of columns to return (pass null to get all)
+            null,              // The columns for the WHERE clause
+            null,          // The values for the WHERE clause
+            null,                   // don't group the rows
+            null,                   // don't filter by row groups
+            sortOrder                   // The sort order
+    );
+    List userName = new ArrayList<>();
+        while(cursor.moveToNext()){
+        String username = cursor.getString(cursor.getColumnIndexOrThrow(UserMaster.Users.COLUMN_NAME_USER_NAME));
+        userName.add(username);
+    }
+        cursor.close();
+        return userName;
+}
+
+    public boolean readInfo(String username, String password){
+        SQLiteDatabase db = getReadableDatabase();
+        boolean value = false;
+        String[] projection = {
+                UserMaster.Users._ID,
+                UserMaster.Users.COLUMN_NAME_USER_NAME,
+                UserMaster.Users.COLUMN_NAME_PASSWORD
+        };
+
+        String selection = UserMaster.Users.COLUMN_NAME_USER_NAME+ " = ?";
+        String[] selectionArgs = { username};
+
+        Cursor cursor = db.query(
+                UserMaster.Users.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                null                   // The sort order
+        );
+
+        while(cursor.moveToNext()){
+            String userName = cursor.getString(cursor.getColumnIndexOrThrow(UserMaster.Users.COLUMN_NAME_USER_NAME));
+            String Passowrd = cursor.getString(cursor.getColumnIndexOrThrow(UserMaster.Users.COLUMN_NAME_PASSWORD));
+
+            value = username.equals(userName) && password.equals(Passowrd);
+        }
+        cursor.close();
+        return value;
+    }
+
+    public int deleteInfo(String username, String password){
+        boolean v = readInfo(username,password);
+        SQLiteDatabase db = getReadableDatabase();
+
+        if(v){
+            String selection = UserMaster.Users.COLUMN_NAME_USER_NAME + " LIKE ?";
+            String[] selectionArgs = { username };
+            int deletedRows = db.delete(UserMaster.Users.TABLE_NAME, selection, selectionArgs);
+
+            return deletedRows;
+        }else{
+            return -1;
+        }
+    }
+
+    public void updateUser(String username, String password){
+            SQLiteDatabase db = getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(UserMaster.Users.COLUMN_NAME_PASSWORD,password);
+
+            String selection = UserMaster.Users.COLUMN_NAME_USER_NAME + " LIKE ?";
+            String[] selectionArgs = { username};
+
+             int count = db.update(
+                UserMaster.Users.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+
     }
 }
--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +11,13 @@ import android.widget.Toast;
 
 import com.example.lab5.Database.DBHandler;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Button addBtn;
+    Button addBtn, selectAll, signIn, deleteBtn ,update;
     DBHandler dbHandler;
     EditText un,pw;
-
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +25,12 @@ public class MainActivity extends AppCompatActivity {
         dbHandler = new DBHandler(this);
 
         addBtn =findViewById(R.id.AddBtn);
+        selectAll =findViewById(R.id.selectAllBtn);
+        signIn =findViewById(R.id.signinBtn);
         un = findViewById(R.id.editTextName);
         pw= findViewById(R.id.editTextPassword);
+        deleteBtn =findViewById(R.id.deleteBtn);
+        update =findViewById(R.id.updateBtn);
 
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +50,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        selectAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List a =dbHandler.ReadAllInfo();
+                Log.i(TAG, "List of users : " + a);
+                Toast.makeText(MainActivity.this, "List printed on the log", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = un.getText().toString();
+                String password = pw.getText().toString();
+
+                boolean a = dbHandler.readInfo(username, password);
+
+                if(a){
+                    Toast.makeText(MainActivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                    un.setText("");
+                    pw.setText("");
+                }else{
+                    Toast.makeText(MainActivity.this, "User is not existing", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = un.getText().toString();
+                String password = pw.getText().toString();
+
+                int b = dbHandler.deleteInfo(username, password);
+                if(b==-1){
+                    Toast.makeText(MainActivity.this, "Fields are not matching", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Deleted successfully", Toast.LENGTH_SHORT).show();
+                    un.setText("");
+                    pw.setText("");
+                }
+            }
+        });
+
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = un.getText().toString();
+                String password = pw.getText().toString();
+                dbHandler.updateUser(username, password);
+                Toast.makeText(MainActivity.this, "Updated successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
